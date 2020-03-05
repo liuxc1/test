92a4ccdfe49271175cf781656e2904d60127386a
liuxc <921119098@qq.com>
2020-03-05 16:34:53 +0800

    提交代码

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author liuxc
  * @date 2020/3/1 - 22:43
@@ -5,7 +8,8 @@
 public class Test {
     public static void main(String[] args) {
 
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-        System.out.println("XXXXXXXXXXXX");
+        System.out.println("OOOKJ");
     }
 }
